65cd380af243992474229e25832f3b408aa2085d
akmitrich <ak.mitrich@mail.ru>
2022-11-13 19:01:42 +0300

    MatrixArray fixed

--- src/lib.rs
+++ src/lib.rs
@@ -10,3 +10,36 @@ pub trait IArray<T> {
     fn insert(&mut self, elem: T, index: usize); // analog of void add(T item, int index)
     fn remove(&mut self, index: usize) -> T;
 }
+
+#[derive(Debug, Default)]
+pub struct Wrapper<T> {
+    inner: Vec<T>,
+}
+
+impl<T> Wrapper<T> {
+    pub fn new() -> Self {
+        Self { inner: vec![] }
+    }
+}
+
+impl<T> IArray<T> for Wrapper<T> {
+    fn size(&self) -> usize {
+        self.inner.len()
+    }
+
+    fn push(&mut self, elem: T) {
+        self.inner.push(elem);
+    }
+
+    fn get(&self, index: usize) -> &T {
+        self.inner.get(index).unwrap()
+    }
+
+    fn insert(&mut self, elem: T, index: usize) {
+        self.inner.insert(index, elem);
+    }
+
+    fn remove(&mut self, index: usize) -> T {
+        self.inner.remove(index)
+    }
+}
--- src/matrix_array.rs
+++ src/matrix_array.rs
@@ -1,11 +1,14 @@
+use std::fmt::Debug;
+
 use crate::{array::Array, vector_array::VectorArray, IArray};
 
-pub struct MatrixArray<T> {
+#[derive(Debug)]
+pub struct MatrixArray<T: Debug> {
     inner: Array<VectorArray<T>>,
     vector: usize,
 }
 
-impl<T> Default for MatrixArray<T> {
+impl<T: Debug> Default for MatrixArray<T> {
     fn default() -> Self {
         Self {
             inner: Default::default(),
@@ -14,7 +17,7 @@ impl<T> Default for MatrixArray<T> {
     }
 }
 
-impl<T> IArray<T> for MatrixArray<T> {
+impl<T: Debug> IArray<T> for MatrixArray<T> {
     fn size(&self) -> usize {
         self.inner
             .iter()
@@ -36,18 +39,34 @@ impl<T> IArray<T> for MatrixArray<T> {
         let (inner_index, single_index) = self.make_indice(index);
         let store = self.inner.get_mut(inner_index).unwrap();
         store.insert(elem, single_index);
+        if store.size() > self.vector {
+            let last_elem = store.remove(store.size() - 1);
+            self.insert(last_elem, (inner_index + 1) * self.vector);
+        }
     }
 
     fn remove(&mut self, index: usize) -> T {
         let (inner_index, single_index) = self.make_indice(index);
         let store = self.inner.get_mut(inner_index).unwrap();
-        store.remove(single_index)
+        let removed = store.remove(single_index);
+        self.shift_after_remove(inner_index);
+        self.check_last_and_remove_if_empty();
+        removed
     }
 }
 
-impl<T> MatrixArray<T> {
+impl<T: Debug> MatrixArray<T> {
     pub fn new(vector: usize) -> Self {
         Self { vector, ..Default::default() }
+    }
+
+    pub fn repr(&self) -> String {
+        let mut result: Vec<String> = vec![String::from("[")];
+        for i in 0..self.inner.size() {
+            result.push(format!("{:?},", self.inner.get(i) as &[T]));
+        }
+        result.push(String::from("]"));
+        result.join("\n")
     }
 
     fn make_indice(&self, index: usize) -> (usize, usize) {
@@ -62,10 +81,27 @@ impl<T> MatrixArray<T> {
 
     fn need_to_grow(&self) -> bool {
         if let Some(last) = self.inner.last() {
-            last.size() == self.vector
-        } else { // inner array is empty
+            last.size() >= self.vector
+        } else {
             true
         }
+    }
+
+    fn check_last_and_remove_if_empty(&mut self) {
+        if let Some(last) = self.inner.last() {
+            if last.is_empty() {
+                let last_index = self.inner.size() - 1;
+                self.inner.remove(last_index);
+            }
+        }
+    }
+
+    fn shift_after_remove(&mut self, inner_index: usize) {
+        for inner_index in (inner_index + 1)..self.inner.size() {
+            let shifted = self.inner[inner_index].remove(0);
+            let prev = self.inner.get_mut(inner_index - 1).unwrap();
+            prev.push(shifted);
+        }   
     }
 }
 
@@ -83,7 +119,7 @@ mod tests {
 
     #[test]
     fn push_get_insert_remove() {
-        let mut array = MatrixArray::<i64>::new(1);
+        let mut array = MatrixArray::<i64>::new(10);
         for i in -42..42 {
             array.push(i);
         }
@@ -91,7 +127,24 @@ mod tests {
         assert_eq!(&0, array.get(42));
         array.insert(686, 42);
         assert_eq!(&686, array.get(42));
+        assert_eq!(&7, array.get(50));
         array.remove(42);
         assert_eq!(&0, array.get(42));
+        for _ in 0..14 {
+            array.remove(42);
+        }
+        println!("{}", array.repr());
+    }
+
+    #[test]
+    fn insert() {
+        let mut bytes = MatrixArray::<u8>::new(3);
+        for i in 0..10 {
+            bytes.push(i);
+        }
+        bytes.insert(42, 4);
+        assert_eq!(0, bytes.remove(0));
+        assert_eq!(6, bytes.remove(6));
+        println!("{}", bytes.repr());
     }
 }
